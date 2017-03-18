81d1f49f4a64e7a5700559aa4d8b36967fbc45ed
rajca <rajca@RAJCANNA>
2017-03-17 21:18:11 -0500

    changes made

---
+++ ConsumeWcf/WcfCalculator.aspx.cs
@@ -0,0 +1,33 @@
+﻿using ConsumeWcf.ProxyWcfCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConsumeWcf
+{
+    public partial class WcfCalculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            CalculatorClient webcalculator = new CalculatorClient("BasicHttpBinding_ICalculator");
+            int addition = webcalculator.add(int.Parse(TextBox1.Text), int.Parse(TextBox2.Text));
+            int subtraction = webcalculator.sub(int.Parse(TextBox1.Text), int.Parse(TextBox2.Text));
+            int division = webcalculator.div(int.Parse(TextBox1.Text), int.Parse(TextBox2.Text));
+            int multiplication = webcalculator.mul(int.Parse(TextBox1.Text), int.Parse(TextBox2.Text));
+
+            TextBox3.Text = addition.ToString();
+            TextBox4.Text = subtraction.ToString();
+            TextBox5.Text = multiplication.ToString();
+            TextBox6.Text = multiplication.ToString();
+        }
+    }
+}
